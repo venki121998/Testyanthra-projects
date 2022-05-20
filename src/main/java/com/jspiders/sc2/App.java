0a8e2c0186a81050f295c64ed1fb87bfba46a8d6
venki121998 <vkvenkatesh1998@gmail.com>
2022-05-20 14:35:32 -0700

    my test yanthra projectprogram

---
+++ src/main/java/com/jspiders/sc2/App.java
@@ -0,0 +1,13 @@
+package com.jspiders.sc2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
